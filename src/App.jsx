1f83ccd0f8b52b61e85cd4717ad26152499d35d7
Ksenia <kudriavtseva_k@mail.ru>
2021-02-12 14:42:23 +0300

    state created with hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import getText from './Api/getText'
+import useKeyPress from './useKeyPress'
 
 const App = () => {
-    const [state, setState] = useState({})
-    const setText = () => {
+    const [text, setText] = useState();
+    const [outgoingValues, setoutgoingValues] = useState();
+    const [incomingValues, setincomingValues] = useState();
+    const [currentSymbol, setCurrentSymbol] = useState();
+    const [startTime, setStartTime] = useState();
+    const [speed, setSpeed] = useState(0);
+    const [accuracy, setAccuracy] = useState(100);
+    const [typedText, setTypedText] = useState('')
+    const onTextButtonClick = () => {
         getText()
-        .then(result => setState({text: result}))
+            .then(result => {
+                setText(result[0]);
+                setincomingValues(result[0].substr(1));
+                setCurrentSymbol(result[0](0));
+            })
+    };
+    const onStartTyping = () => {
+        setStartTime(new Date().getTime())
+    }
+    const onFinishTyping = () => {
+        const finishTime = new Date().getTime()
+        setSpeed((finishTime - startTime)/60000)
+    }
+    const onTyping = (key) => {
+        setTypedText(typedText + key)
+        setAccuracy( (outgoingValues.length * 100) / (typedText + key).length)
     }
     
-    
+
     return <div className="app">
         <div className="app__content">
-            <button className="restart" onClick={setText}>
+            <button className="restart" onClick={onTextButtonClick}>
                 Задать текст
             </button>
             <div className="textarea">
-                {state.text || ''}
+                {text || ''}
             </div>
             <div className="result">
                 <div>
-                    Скорость (знаков в минуту):
+                    Скорость (знаков в минуту): {speed}
                 </div>
                 <div>
-                    Точность (процент правильных символов):
+                    Точность (процент правильных символов): {accuracy}%
                 </div>
             </div>
             <button className="restart">Начать заново</button>
