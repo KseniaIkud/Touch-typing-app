7383081e4cd18444840fa78ebc1d7086febc43ce
Ksenia <kudriavtseva_k@mail.ru>
2021-02-14 00:21:15 +0300

    styles updated for start component

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Jumbotron, Button } from 'react-bootstrap';
 
 const Start = () => {
     return (
-        <Jumbotron>
+        <Jumbotron className="w-75 mt-5 mx-auto border">
             <h1>Проверьте скорость своей печати</h1>
             <p>
                 Наберите небольшой текст, чтобы узнать скорость и точность набора
             </p>
             <p>
-                <Button variant="primary">Начать</Button>
+                <Button variant="outline-primary">Начать</Button>
             </p>
         </Jumbotron>
     )
