fd950fd08b39b9f151482cc044c1e9b2c0cbecce
Ksenia <kudriavtseva_k@mail.ru>
2021-02-14 16:56:15 +0300

    absolute position added to Result component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,23 +4,23 @@ import {Alert, Button} from 'react-bootstrap';
 
 const Result = (props) => {
     return (
-        <>
-            <Alert show={props.show} variant="success">
+        <div className={props.show ? "w-100 h-75 position-absolute d-flex justify-content-center" : "d-none"}>
+            <Alert show={props.show} variant="success" className="w-25 align-self-center">
                 <Alert.Heading>Ваш результат</Alert.Heading>
                 <p>
                     {props.speed} знаков в минуту
                 </p>
                 <p>
-                    {props.accuracy} % точности
+                    {props.accuracy}% точности
                 </p>
                 <hr />
-                <div className="d-flex justify-content-start">
+                <div className="d-flex justify-content-center">
                 <Button onClick={props.onStart} variant="outline-success">
                     Начать заново
                 </Button>
                 </div>
             </Alert>
-        </>
+        </div>
     )
 }
 
