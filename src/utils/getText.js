04435502212d1ad945075a7baf5421b961aacc6d
Ksenia <kudriavtseva_k@mail.ru>
2021-02-14 19:20:12 +0300

    api request for cyrillic text added

--- src/utils/getText.js
+++ src/utils/getText.js
@@ -1,5 +1,13 @@
-const getText = () => {
-    return fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1')
+const getText = {
+    getLatinText() {
+        return fetch('https://baconipsum.com/api/?type=all-meat&sentences=1&start-with-lorem=1')
         .then(response => response.json())
+        .then(array => array[0])
+    },
+    getCyrillicText() {
+        return fetch('https://fish-text.ru/get?format=json&number=3')
+        .then(response => response.json())
+        .then(object => object.text)
+    }
 }
 export default getText
