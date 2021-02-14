f8ea07bf7d219283bb389e1a9c07c8ac563796b0
Ksenia <kudriavtseva_k@mail.ru>
2021-02-14 15:04:29 +0300

    code refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,11 +9,6 @@ import TypingArea from './TypingArea';
 import Result from './Result';
 
 const App = () => {
-
-    const[result2, setResult2] = useState({
-        speed: 0,
-        accuracy: 100
-    });
     
     const [outgoingValues, setOutgoingValues] = useState('');
     const [incomingValues, setIncomingValues] = useState();
@@ -23,8 +18,9 @@ const App = () => {
     const [accuracy, setAccuracy] = useState(100);
     const [typedText, setTypedText] = useState('');
     const [isWrongSymbol, setWrongSymbol] = useState(false);
-    const [showResult, setShowResult] = useState (false);
     const [showStart, setShowStart] = useState(true);
+    const [showResult, setShowResult] = useState (false);
+    
     const updateState = () => {
         setOutgoingValues('');
         setStartTime();
@@ -34,32 +30,29 @@ const App = () => {
         setWrongSymbol(false);
     }
     const onStart = () => {
-        setShowResult(false);
         setShowStart(false);
+        setShowResult(false);
+        updateState();
         getText()
             .then(result => {
                 let wholeText = result[0];
                 setIncomingValues(wholeText.substr(1));
                 setCurrentSymbol(wholeText.charAt(0));
-                updateState();
             })
             .catch(err => console.log(err))
     };
-    const updateAccuracy = (expected, typed) => {
-        setResult2({
-            ...result2,
-            accuracy: ((expected.length * 100) / (typed.length)).toFixed(0,)
-        })
-        // setAccuracy(((expected.length * 100) / (typed.length)).toFixed(0,));
-    }
 
-    useKeyPress((key) => {
+    const onKeyPress = (key) => {
+        const updateAccuracy = (expected, typed) => {
+            setAccuracy(((expected.length * 100) / (typed.length)).toFixed(0,));
+        }
         let currentTime = getCurrentTime();
         if (!startTime) {
             setStartTime(currentTime);
         };
         let updatedTypedText = typedText + key;
         let updatedOutgoingValues = outgoingValues;
+
         if (key === currentSymbol) {
             setTypedText(updatedTypedText);
 
@@ -70,13 +63,15 @@ const App = () => {
             setIncomingValues(incomingValues.substr(1));
             setWrongSymbol(false);
             updateAccuracy(updatedOutgoingValues, updatedTypedText);
-            if (startTime && !incomingValues) {
+            let isFinished = (startTime && !incomingValues)
+            if (isFinished) {
                 setOutgoingValues('');
-                setShowResult(true)
+                setShowResult(true);
             }
         } else {
             setWrongSymbol(true);
-            if (updatedTypedText.slice(-2, -1) === updatedOutgoingValues.slice(-1)) {
+            let isSameMistake = (updatedTypedText.slice(-2, -1) === updatedOutgoingValues.slice(-1))
+            if (isSameMistake) {
                 setTypedText(updatedTypedText);
                 updateAccuracy(updatedOutgoingValues, updatedTypedText);
             }
@@ -86,12 +81,15 @@ const App = () => {
             const speed = (updatedOutgoingValues.length / duration).toFixed(0,);
             setSpeed(speed);
         }
-    })
+    }
+    
+
+    useKeyPress(onKeyPress)
 
 
     return <div className="app">
         <Start show={showStart} onStart={onStart}/>
-        <TypingArea outgoingValues={outgoingValues} currentSymbol={currentSymbol} incomingValues={incomingValues} isWrong={isWrongSymbol} speed={result2.speed} accuracy={result2.accuracy} />
+        <TypingArea outgoingValues={outgoingValues} currentSymbol={currentSymbol} incomingValues={incomingValues} isWrong={isWrongSymbol} speed={speed} accuracy={accuracy} />
         <Result speed={speed} accuracy={accuracy} show={showResult} onStart={onStart}/>
     </div>
 }
