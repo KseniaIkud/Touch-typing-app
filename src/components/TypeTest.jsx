84b00d26ba4cc9dcdb494a6cf295d5df52f37d39
Ksenia <kudriavtseva_k@mail.ru>
2021-02-16 14:08:36 +0300

    code refactoring

--- src/components/TypeTest.jsx
+++ src/components/TypeTest.jsx
@@ -1,25 +1,24 @@
 import React, { useContext } from 'react';
-import {Store} from '../store/reducer';
+import { Store } from '../store/reducer';
+import ACTIONS from '../store/actions';
 
 import getText from '../utils/getText';
 import getCurrentTime from '../utils/getCurrentTime';
-
 import useKeyPress from '../hooks/useKeyPress';
-import ACTIONS from '../store/actions';
 
 import Start from './Start';
 import TypingArea from './TypingArea';
 import Result from './Result';
 
 const TypeText = () => {
-    const {state, dispatch} = useContext(Store);
+    const { state, dispatch } = useContext(Store);
 
     const onStart = () => {
-        dispatch({ type: ACTIONS.RESET_STATE })
+        dispatch({ type: ACTIONS.RESET_STATE });
         getText(state.language)
             .then(result => {
-                dispatch({ type: ACTIONS.SET_TEXT, result })
-            })
+                dispatch({ type: ACTIONS.SET_TEXT, result });
+            });
     };
 
     const onKeyPress = (key) => {
@@ -27,32 +26,33 @@ const TypeText = () => {
 
         let isTyping = state.startTime && state.outgoingValues;
         if (isTyping) {
-            dispatch({ type: ACTIONS.SET_SPEED, time: getCurrentTime() })
-        }
-        
+            dispatch({ type: ACTIONS.SET_SPEED, time: getCurrentTime() });
+        };
+
         if (key === state.currentSymbol) {
-            dispatch({ type: ACTIONS.SET_RIGHT_KEY, key })
-            let isFinished = (state.startTime && !state.incomingValues)
+            dispatch({ type: ACTIONS.SET_RIGHT_KEY, key });
+
+            let isFinished = (state.startTime && !state.incomingValues);
             if (isFinished) {
-                dispatch({ type: ACTIONS.COMPLETE })
-            }
-            
+                dispatch({ type: ACTIONS.COMPLETE });
+            };
+
         } else {
-            dispatch({ type: ACTIONS.WRONG_SYMBOL })
+            dispatch({ type: ACTIONS.WRONG_SYMBOL });
 
             let isMistakeNew = state.typedText.substr(-1) === state.outgoingValues.substr(-1);
             if (isMistakeNew) {
-                dispatch({ type: ACTIONS.SET_WRONG_KEY, key })
-            }
-        }
-    }
+                dispatch({ type: ACTIONS.SET_WRONG_KEY, key });
+            };
+        };
+    };
 
-    useKeyPress(onKeyPress)
+    useKeyPress(onKeyPress);
 
     return <div>
-        <Start onStart={onStart}/>
-        <Result onStart={onStart}/>
-        <TypingArea onStart={onStart}/>
+        <Start onStart={onStart} />
+        <Result onStart={onStart} />
+        <TypingArea onStart={onStart} />
     </div>
 }
 
